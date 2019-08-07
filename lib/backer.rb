59fdbd1a4d7d49e4ed7396cae61c26d47ecb4e7a
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-07 22:53:12 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,7 +1,6 @@
 class Backer
-  attr_reader :name
-  @@backers = []
-  @@backed_projects = []
+  attr_reader :name  
+  @backed_projects = []
   def initialize(name)
     @name = name
 
@@ -9,12 +8,11 @@ class Backer
 
 
   def back_project(project)
-    @@backed_projects << project
-    project.add_backer(self)
+    @backed_projects << project
   end
 
   def backed_projects
-    @@backed_projects
+    @backed_projects
   end
 
 
